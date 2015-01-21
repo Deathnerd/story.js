df0ec6c0885c0a7cd84b2153fafccd2008c11d07
deathnerd <wes.gilleland@gmail.com>
2015-01-21 19:16:09 +0000

    initial commit

---
+++ story.js
@@ -0,0 +1,13 @@
+Story = {
+    
+    init: function(){
+        //load the cookie
+        
+        //if no cookie, ask for login
+    },
+    
+    
+    login: function(name) {
+        
+    }
+}
