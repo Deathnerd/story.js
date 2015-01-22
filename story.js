b8562ffe69aa546024f2bad0c79d4af5896625e7
Wes Gilleland <wes.gilleland@gmail.com>
2015-01-21 23:31:51 -0500

    Initial commit

--- story.js
+++ story.js
@@ -1,13 +1,60 @@
+String.prototype.has = function(foo){
+	return this.indexOf(foo) != -1;
+};
+
+Cookie = {
+
+	init: function(){
+
+	},
+
+	data: {
+
+	},
+	save: function(){
+
+	}
+};
+
+
 Story = {
-    
+
+	cookie: "",
+
     init: function(){
         //load the cookie
-        
-        //if no cookie, ask for login
+        this.cookie = document.cookie;
+
+	    //if no cookie, ask for login
+	    if(this.cookie.length === 0 || this.cookie.has("story_id")){
+		    this.login();
+	    }
     },
-    
-    
-    login: function(name) {
-        
-    }
-}
+
+    login: function() {
+	    var story_id = prompt("Enter your Story Id to pick up where you left off or cancel to create a new game");
+	    if(story_id === null || story_id === ""){
+			this.cookie = "story_id:" + Math.round(Math.random()*1000) + "|";
+		    this.cookie += "current_page:" + this._getCurrentPageName();
+	    }
+    },
+
+	save: function() {
+		var page = this._getCurrentPageName();
+	},
+
+	_getCurrentPageName: function(){
+		var path = window.location.pathname;
+		var page = path.split("/");
+
+		if(page.length == 1){
+			return "index";
+		}
+
+		return page.pop().split(".")[0];
+	},
+
+	_buildCookie: function(){
+
+	}
+};
---
+++ story_class.js
@@ -0,0 +1,13 @@
+/**
+ * A class to handle the operation of a simple choose your own story game
+ * @author Wes Gilleland
+ * @type {Class}
+ */
+var Story = new Class({
+	/**
+	 * Constructor!
+	 */
+	create: function(){
+
+	}
+});
